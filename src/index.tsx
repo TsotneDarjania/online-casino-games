feed3bb742632712e9172ed9d59278b39897cc06
= <=>
2023-05-20 03:08:16 +0400

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import JunkgleAdventure from "./games/slots/jungle-adventure/index.tsx";
 import Mines from "./games/pazzle/mines/index.tsx";
 
-console.log("index tsx");
+console.log("index tsx browser router");
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <Router>
+  <BrowserRouter>
     <Routes>
       <Route path="/" element={<Mines />} />
       <Route path="/jungle" element={<JunkgleAdventure />} />
       <Route path="/mines" element={<Mines />} />
     </Routes>
-  </Router>
+  </BrowserRouter>
 );
