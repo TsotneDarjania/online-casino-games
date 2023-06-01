4537aa1e000f31d4e22fd7597e865128a8d2a075
= <=>
2023-06-01 23:37:16 +0400

    .

--- src/games/slots/jungle-adventure/index.tsx
+++ src/games/slots/jungle-adventure/index.tsx
@@ -30,8 +30,8 @@ const JunkgleAdventure = () => {
 
         // mode: Phaser.Scale.FIT,
         // autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: document.body.clientWidth,
-        height: document.body.clientHeight,
+        width: document.body.offsetWidth,
+        height: document.body.offsetHeight,
       },
       backgroundColor: 0xc322e3,
       scene: [Preload, Main],
