f5e0571cec487c32ca17b6e19eb4664ba4752100
= <=>
2023-06-03 21:21:54 +0400

    .

--- src/games/slots/jungle-adventure/scenes/main.ts
+++ src/games/slots/jungle-adventure/scenes/main.ts
@@ -111,7 +111,6 @@ export class Main extends Phaser.Scene {
 
     this.scale.on(Phaser.Scale.Events.ENTER_FULLSCREEN, () => {
       this.pressToStart.setVisible(false);
-      // this.changeOrientationSize(window.outerWidth, window.outerHeight);
 
       this.scale.removeAllListeners();
       this.scene.restart();
@@ -124,13 +123,7 @@ export class Main extends Phaser.Scene {
       )
       .on(Phaser.Input.Events.POINTER_UP, () => {
         this.pressToStart.setVisible(false);
-        // this.scale.removeAllListeners();
-
-        // this.scene.restart();
-
-        // setTimeout(() => {
-
-        // }, 1000);
+        this.changeOrientationSize(window.outerWidth, window.outerHeight);
 
         this.scale.startFullscreen();
       });
